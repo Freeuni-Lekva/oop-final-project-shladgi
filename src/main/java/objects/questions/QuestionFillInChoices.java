5275ff5eed3481c06c46ced36934d397bcb5194e
tornike-MACS <your_email@example.com>
2025-06-15 18:00:51 +0400

    starter codes without tests

---
+++ src/main/java/objects/questions/QuestionFillInChoices.java
@@ -0,0 +1,104 @@
+package objects.questions;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionFillInChoices extends Question{
+    private List<Integer> correctIndexes;
+    private List<List<String>> choices;
+    private List<Integer> fillIndexes;
+
+    public QuestionFillInChoices(String question, List<Integer> correctIndexes, List<List<String>> choices, List<Integer> fillIndexes, int quizId){
+        this.question = question;
+        this.type = QType.FillChoices;
+        this.maxScore = correctIndexes.size();
+        this.correctIndexes = new ArrayList<>(correctIndexes);
+        this.choices = new ArrayList<>();
+        for(int i = 0; i < choices.size(); i++){
+            this.choices.add(new ArrayList<>(choices.get(i)));
+        }
+        this.fillIndexes = new ArrayList<>(fillIndexes);
+    }
+
+    // this is a constructor that constructs the object from database table information.
+    public QuestionFillInChoices(int id, int quizId, String question, String imageLink, int maxScore, JsonObject json) {
+        super(id,quizId,question,imageLink,maxScore,json, QType.FillChoices);
+    }
+
+    @Override
+    public int check(Answer<?> answer) {
+        int points = 0;
+        for(int i = 0; i < this.correctIndexes.size(); i++){
+            if(this.correctIndexes.get(i) == (int)answer.get(i)) points++;
+        }
+        return points;
+    }
+
+    @Override
+    public JsonObject getData() {
+        JsonObject jsonObject = new JsonObject();
+
+        JsonArray jsonCorrectIndexes = new JsonArray();
+        for(Integer index : this.correctIndexes){
+            jsonCorrectIndexes.add(index);
+        }
+
+        jsonObject.add("correctIndexes", jsonCorrectIndexes);
+
+        JsonArray jsonChoices = new JsonArray();
+
+        for(int i = 0; i < this.choices.size(); i++){
+            JsonArray eachBlank = new JsonArray();
+
+            for(int j = 0; j < this.choices.get(i).size(); j++){
+                eachBlank.add(this.choices.get(i).get(j));
+            }
+
+            jsonChoices.add(eachBlank);
+        }
+
+        jsonObject.add("choices", jsonChoices);
+
+        JsonArray jsonBlanks = new JsonArray();
+
+        for(int i = 0; i < this.fillIndexes.size(); i++){
+            jsonBlanks.add(this.fillIndexes.get(i));
+        }
+
+        jsonObject.add("blanks", jsonBlanks);
+
+        return jsonObject;
+    }
+
+    @Override
+    public void putData(JsonObject json) {
+        this.correctIndexes = new ArrayList<>();
+        JsonArray jsonCorrectIndexes = json.get("correctIndexes").getAsJsonArray();
+        for(int i = 0; i < jsonCorrectIndexes.size(); i++){
+            this.correctIndexes.add(jsonCorrectIndexes.get(i).getAsInt());
+        }
+
+        this.choices = new ArrayList<>();
+        JsonArray jsonChoices = json.get("choices").getAsJsonArray();
+        for(int i = 0; i < jsonChoices.size(); i++){
+            JsonArray jsonChoice = jsonChoices.get(i).getAsJsonArray();
+            List<String> choice = new ArrayList<>();
+            for(int j = 0; j < jsonChoice.size(); j++){
+                choice.add(jsonChoice.get(j).getAsString());
+            }
+            this.choices.add(choice);
+        }
+
+        this.fillIndexes = new ArrayList<>();
+
+        JsonArray jsonBlanks = json.get("blanks").getAsJsonArray();
+
+        for(int i = 0; i < jsonBlanks.size(); i++){
+            fillIndexes.add(jsonBlanks.get(i).getAsInt());
+        }
+
+    }
+}
---
+++ src/main/java/objects/questions/QuestionMultiTextAnswer.java
@@ -0,0 +1,111 @@
+package objects.questions;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionMultiTextAnswer extends Question{
+    List<List<String>> correctAnswers;
+    boolean exactMatch;
+
+    public QuestionMultiTextAnswer(String question, List<List<String>> correctAnswers, boolean exactMatch){
+        this.question = question;
+
+        this.correctAnswers = new ArrayList<>();
+        for(int i = 0; i < correctAnswers.size(); i++){
+            correctAnswers.add(new ArrayList<>(correctAnswers.get(i)));
+        }
+        this.exactMatch = exactMatch;
+    }
+
+    // this is a constructor that constructs the object from database table information.
+    public QuestionMultiTextAnswer(int id, int quizId, String question, String imageLink, int maxScore, JsonObject json) {
+        super(id,quizId,question,imageLink,maxScore,json, QType.MultiTextAnswer);    }
+
+    @Override
+    public int check(Answer<?> answer) {
+        if (!(answer.get(0) instanceof String)) return 0;
+
+        List<String> userAnswers = new ArrayList<>();
+        for (int i = 0; i < answer.getSize(); i++) {
+            userAnswers.add((String) answer.get(i));
+        }
+
+        boolean[] used = new boolean[correctAnswers.size()];
+        int points = 0;
+
+        for (String userAnswer : userAnswers) {
+            boolean matched = false;
+
+            for (int i = 0; i < correctAnswers.size(); i++) {
+                if (used[i]) continue;
+
+                for (String possible : correctAnswers.get(i)) {
+                    if (myCheck(possible, userAnswer)) {
+                        used[i] = true;
+                        points++;
+                        matched = true;
+                        break;
+                    }
+                }
+
+                if (matched) break;
+            }
+        }
+
+        return points;
+    }
+
+    private boolean myCheck(String possibleAnswer, String answer){
+        if(this.exactMatch){
+            return possibleAnswer.equals(answer);
+        }
+
+        answer = answer.toLowerCase();
+        // \\s+ is equals to " ".
+        answer = answer.replaceAll("\\s+","");
+
+        possibleAnswer = possibleAnswer.toLowerCase();
+        possibleAnswer = possibleAnswer.replaceAll("\\s+","");
+
+        return answer.equals(possibleAnswer);
+
+    }
+
+    @Override
+    public JsonObject getData() {
+        JsonObject jsonObject = new JsonObject();
+
+        jsonObject.addProperty("exactMatch", this.exactMatch);
+
+        JsonArray jsonArray = new JsonArray();
+        for(int i = 0; i < this.correctAnswers.size(); i++){
+            JsonArray each = new JsonArray();
+            for(int j = 0; j < this.correctAnswers.get(i).size(); j++){
+                each.add(correctAnswers.get(i).get(j));
+            }
+            jsonArray.add(each);
+        }
+
+        jsonObject.add("correctAnswers", jsonArray);
+
+        return null;
+    }
+
+    @Override
+    public void putData(JsonObject json) {
+        this.exactMatch = json.get("exactMatch").getAsBoolean();
+        JsonArray jsonArray = json.get("correctAnswers").getAsJsonArray();
+
+        for(int i = 0; i < jsonArray.size(); i++){
+            JsonArray jsonEach = jsonArray.get(i).getAsJsonArray();
+            List<String> each = new ArrayList<>();
+            for(int j = 0; j < jsonEach.size(); j++){
+                each.add(jsonEach.get(i).getAsString());
+            }
+            this.correctAnswers.add(each);
+        }
+    }
+}
