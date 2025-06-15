5a2d1c8a82c43c7a16a4d2a8423cefcd7ec723b1
Gio Mamatsashvili <gmama23@freeuni.edu.ge>
2025-06-15 16:01:20 +0400

    textAnswer fixed

--- src/main/java/objects/questions/QuestionTextAnswer.java
+++ src/main/java/objects/questions/QuestionTextAnswer.java
@@ -7,20 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionTextAnswer extends Question {
-    private boolean oneAns;
+    private boolean oneAns, exactMatch;
     private String correctAnswer;
     private List<String> correctAnswers;
 
-    public QuestionTextAnswer(String question, String correctAnswer) {
-        this.oneAns = true;
-        this.question = question;
-        this.correctAnswer = correctAnswer.toLowerCase();
-        this.correctAnswers = new ArrayList<>();
-        this.correctAnswers.add(this.correctAnswer);
-    }
-
-    public QuestionTextAnswer(String question, List<String> correctAnswers) {
+    public QuestionTextAnswer(String question, List<String> correctAnswers, boolean exactMatch) {
         this.oneAns = false;
+        this.type = QType.TextAnswer;
+        this.maxScore = 1;
         this.question = question;
         this.correctAnswers = new ArrayList<>();
         for (String s : correctAnswers) {
@@ -31,12 +25,9 @@ public class QuestionTextAnswer extends Question {
     @Override
     public int check(Answer<?> answer) {
         if (oneAns) return answer.get().toString().toLowerCase().equals(correctAnswer) ? 1 : 0;
-        List<String> remaining = new ArrayList<>(correctAnswers);
-        for (int i = 0; i < answer.getSize(); i++) {
-            String userAnswer = answer.get(i).toString().toLowerCase();
-            if (!remaining.remove(userAnswer)) return 0;
-        }
-        return 1;
+        for(int i = 0; i < correctAnswers.size(); i++)
+            if(myEquals((String) answer.get(), correctAnswers.get(i)))return 1;
+        return 0;
     }
 
     @Override
@@ -52,12 +43,25 @@ public class QuestionTextAnswer extends Question {
     public void putData(JsonObject json) {
         this.correctAnswers = new ArrayList<>();
         JsonArray jsonArray = json.getAsJsonArray("correctAnswers");
-        for (int i = 0; i < jsonArray.size(); i++) {
-            this.correctAnswers.add(jsonArray.get(i).getAsString().toLowerCase());
-        }
+        for (int i = 0; i < jsonArray.size(); i++)
+            this.correctAnswers.add(jsonArray.get(i).getAsString());
+
         this.oneAns = correctAnswers.size() == 1;
-        if (oneAns) {
-            this.correctAnswer = correctAnswers.get(0);
+        if (oneAns) this.correctAnswer = correctAnswers.get(0);
+    }
+
+    private boolean myEquals(String answer, String correctAnswer){
+        if(exactMatch){
+            return answer.equals(correctAnswer);
         }
+
+        answer = answer.toLowerCase();
+        // \\s+ is equals to " ".
+        answer = answer.replaceAll("\\s+","");
+
+        correctAnswer = correctAnswer.toLowerCase();
+        correctAnswer = correctAnswer.replaceAll("\\s+","");
+
+        return answer.equals(correctAnswer);
     }
 }
