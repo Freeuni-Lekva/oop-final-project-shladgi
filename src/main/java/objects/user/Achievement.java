d38ad8756aec741bc03411f9d1f3da1c16544123
gradnos <sdolaqidze@gmail.com>
2025-06-22 12:57:58 +0400

    ADD missing variables

--- src/main/java/objects/user/Achievement.java
+++ src/main/java/objects/user/Achievement.java
@@ -2,6 +2,8 @@ package objects.user;
 
 public class Achievement {
     private String id;
+    private String title;
+    private String description;
     private String iconLink;
     private AchievementRarity achivementRarity;
 
@@ -14,4 +16,6 @@ public class Achievement {
     public String getId() {return id;}
     public String getIconLink() {return iconLink;}
     public AchievementRarity getAchivementRarity() {return achivementRarity;}
+    public String getTitle() {return title;}
+    public String getDescription() {return description;}
 }
