8ed60b638e2b4035fc4cfe0fc94c0f443f21b32f
tornike-MACS <your_email@example.com>
2025-06-24 17:57:24 +0400

    login servlet, context listener

---
+++ src/main/java/routes/AppContextListener.java
@@ -0,0 +1,15 @@
+package routes;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce){
+        //TODO
+        //initialize necessary items
+    }
+}
---
+++ src/main/java/routes/LoginServlet.java
@@ -0,0 +1,26 @@
+package routes;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "loginServlet", value = "/login-servlet")
+public class LoginServlet extends HttpServlet {
+
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        //TODO
+        //handle login
+
+    }
+
+}
