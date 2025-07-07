ce34af29ae07e90ebafb1045f1acbc5a0f33bb5a
lukaLomadze <lloma23@freeuni.edu.ge>
2025-07-07 17:58:26 +0400

    changed navbar

--- src/main/webapp/javascript/fetchUserId.js
+++ src/main/webapp/javascript/fetchUserId.js
@@ -1,12 +1,12 @@
-import { loadSessionValue } from "./getSessionInfo.js";
-window.addEventListener("DOMContentLoaded", async () => {
-    console.log("------------------------------------")
-    const userid = await loadSessionValue("userid");
-    const welcome = document.getElementById("welcomeMsg");
-
-    if (userid) {
-        welcome.textContent = `Hello, user #${userid}!`;
-    } else {
-        welcome.textContent = "Hello, guest!";
-    }
-});
+// import { loadSessionValue } from "./getSessionInfo.js";
+// window.addEventListener("DOMContentLoaded", async () => {
+//     console.log("------------------------------------")
+//     const userid = await loadSessionValue("userid");
+//     const welcome = document.getElementById("welcomeMsg");
+//
+//     if (userid) {
+//         welcome.textContent = `Hello, user #${userid}!`;
+//     } else {
+//         welcome.textContent = "Hello, guest!";
+//     }
+// });
--- src/main/webapp/javascript/navFoot.js
+++ src/main/webapp/javascript/navFoot.js
@@ -1,12 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('nav.html')
-        .then(res => res.text())
-        .then(data => {
-            document.querySelector('nav').innerHTML = data;
-        });
-    fetch('footer.html')
-        .then(res => res.text())
-        .then(data => {
-            document.querySelector('footer').innerHTML = data;
-        });
+import { loadSessionValue } from "./getSessionInfo.js";
+
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+    const navResponse = await fetch('nav.html');
+    const navHtml = await navResponse.text();
+    document.querySelector('nav').innerHTML = navHtml;
+
+    const footerResponse = await fetch('footer.html');
+    const footerHtml = await footerResponse.text();
+    document.querySelector('footer').innerHTML = footerHtml;
+
+    const userid = await loadSessionValue("userid");
+    const userName = await loadSessionValue("username");
+    const userType = await loadSessionValue("type");
+    console.log(userType);
+    const navLinks = document.getElementById("links");
+
+    if (userType) {
+        navLinks.innerHTML += `
+            <li class="nav-item" id="user">
+                <a class="nav-link" href="user.html">${userName}</a>
+            </li>`;
+        if (userType === "Admin") {
+            navLinks.innerHTML += `
+                <li class="nav-item" id="admin">
+                    <a class="nav-link" href="admin.html">Admin Panel</a>
+                </li>`;
+        }
+        navLinks.innerHTML+=
+            `
+                <li class="nav-item" id="logout">
+                    <a class="nav-link" href="logout.html">Log Out</a>
+                </li>`;
+    } else {
+        navLinks.innerHTML += getLi("login","Log In");
+    }
 });
+
+
+function getLi(id,txt){
+    return `<li class="nav-item" id=${id}>
+            <a class="nav-link" href="/login">${txt}</a></li>`
+}
+
+
+
