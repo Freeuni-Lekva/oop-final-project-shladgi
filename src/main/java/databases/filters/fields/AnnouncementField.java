15dcf25de556357219871c4703238f4db53126c4
lukaLomadze <lloma23@freeuni.edu.ge>
2025-07-11 09:23:20 +0400

    added promotion

--- src/main/java/databases/filters/fields/AnnouncementField.java
+++ src/main/java/databases/filters/fields/AnnouncementField.java
@@ -3,7 +3,9 @@ package databases.filters.fields;
 public enum AnnouncementField implements SqlField{
     ID("id"),
     TITLE("title"),
+    AUTHOR("author"),
     CREATIONDATE("creationdate");
+
 
     private String columnName;
     AnnouncementField(String columnName){
