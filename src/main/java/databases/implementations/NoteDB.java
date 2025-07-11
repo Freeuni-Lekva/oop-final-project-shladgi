f195c372dcdbe32c39b02c0cd40d5fa2a2266042
lukaLomadze <lloma23@freeuni.edu.ge>
2025-07-11 05:33:57 +0400

    finished notifications page

--- src/main/java/databases/implementations/NoteDB.java
+++ src/main/java/databases/implementations/NoteDB.java
@@ -1,11 +1,25 @@
 package databases.implementations;
 
 import databases.DataBase;
+import databases.filters.FilterBuilder;
+import databases.filters.FilterCondition;
 import databases.filters.fields.NoteField;
 import objects.user.Note;
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
 
 public class NoteDB extends DataBase<Note, NoteField> {
     public NoteDB(Connection connection) {super(connection, Note.class);}
+
+    public void updateViewed(List<FilterCondition<NoteField>> filterConditions, boolean viewed) {
+        String filterString = FilterBuilder.buildFilter(filterConditions);
+        try (PreparedStatement stmt = con.prepareStatement("UPDATE " + tableName + " SET  viewed = "+ viewed +   " WHERE " + filterString)) {
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("update ERROR \n" + e.getMessage());
+        }
+
+
+    }
 }
